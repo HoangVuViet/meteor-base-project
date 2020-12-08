906c919cb01d46b1bdd5e6f6c4c07fa33b3d93ee
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-12-08 14:46:08 +0700

    delete comment

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-// export const App = () => (
-//   <div>
-//     <h1>Welcome to Meteor!</h1>
-//   </div>
-// );
-
 const App: React.FC = () => {
   return <div>
     <h1>
