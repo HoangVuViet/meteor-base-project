423780741d7088bb8f5b83fcc59890918164bb35
HoagDev <hoangk62uet@gmail.com>
2021-01-29 00:05:54 +0700

    fix

---
+++ private/download_landsat.py
@@ -0,0 +1,38 @@
+import sys
+import landsatxplore.api
+from landsatxplore.earthexplorer import EarthExplorer
+from download_param import *
+
+def download_landsat(start_date,end_date,spatial_range,out_path):
+    """[This function to download Landsat product]
+
+    Args:
+        start_date ([String]): [Start date to download]
+        end_date ([String]): [End date to download]
+        spatial_range([collection]):[(ymin,xmin,ymax,xmax) spatial range to download]
+        output_path ([String]): [Local folder to save downloaded file]
+        
+    """
+    # Initialize a new API instance and get an access key
+    api = landsatxplore.api.API(LANDSAT_USER, LANDSAT_PASS)
+
+    # Request
+    scenes = api.search(
+        dataset = LANDSAT_PRODUCT,
+        bbox= spatial_range,
+        start_date = start_date,
+        end_date = end_date,
+        max_cloud_cover = 10)
+
+    print('{} scenes found.'.format(len(scenes)))
+    api.logout()
+
+    ee = EarthExplorer(LANDSAT_USER, LANDSAT_PASS)
+
+    for scene in scenes:
+        print(scene['acquisitionDate'])
+        ee.download(scene_id=scene['entityId'], output_dir=out_path)
+        
+    ee.logout()
+download_landsat(LANDSAT_START_DATE,LANDSAT_END_DATE,LANDSAT_RANGE,LANDSAT_OUT_PATH)
+#download_landsat(sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3])
