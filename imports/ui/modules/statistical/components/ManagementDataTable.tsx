bd00475c1a0f69a6eb996c160141a06268998b2e
Vũ Việt Hoàng <hoangk62uet@gmail.com>
2021-05-24 23:29:56 +0700

    updte codedsdfsd

--- imports/ui/modules/statistical/components/ManagementDataTable.tsx
+++ imports/ui/modules/statistical/components/ManagementDataTable.tsx
@@ -1,4 +1,19 @@
-import { Button, Dialog, DialogContent, Divider, IconButton, Typography } from '@material-ui/core';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  Divider,
+  IconButton,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+import {
+  DataGrid,
+  GridColDef,
+  GridRowData,
+  GridToolbar,
+  GridValueGetterParams,
+} from '@material-ui/data-grid';
 import AddIcon from '@material-ui/icons/Add';
 import IconClose from '@material-ui/icons/CloseOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -7,16 +22,14 @@ import { Form, Formik } from 'formik';
 import moment from 'moment';
 import { useSnackbar } from 'notistack';
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import ConfirmDialog from '../../common/components/ConfirmDialog';
 import { Col, Row, snackbarSetting } from '../../common/components/elements';
 import LoadingButton from '../../common/components/LoadingButton';
-import TableCustom, { Column } from '../../common/components/TableCustom';
 import { filterList } from '../utils';
 import AddDataDialog from './AddDataDialog';
 import EditDataDialog from './EditDataDialog';
-import Filter from './Filter';
-import { GREY, GREY_600 } from '/imports/ui/configs/colors';
+import { GREY } from '/imports/ui/configs/colors';
 import { isEmpty, some } from '/imports/ui/constants';
 import { DATE_FORMAT, DATE_TIME_FORMAT } from '/imports/ui/models/moment';
 
@@ -31,8 +44,7 @@ interface ITableProps {
 }
 
 const Table: React.FunctionComponent<ITableProps> = (props) => {
-  const { loading, dataList, setFilter, filter, handleDeleteData, handleEditData, handleAddData } =
-    props;
+  const { dataList, handleDeleteData, handleEditData, handleAddData, loading } = props;
 
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
@@ -40,73 +52,97 @@ const Table: React.FunctionComponent<ITableProps> = (props) => {
   const [editOpen, setEditOpen] = React.useState(false);
   const [addOpen, setAddOpen] = React.useState(false);
   const [rowData, setRowData] = React.useState<some>({});
+  const intl = useIntl();
 
-  const columns = React.useMemo(() => {
-    const temp: Column[] = [
+  const columns: GridColDef[] = React.useMemo(() => {
+    return [
       {
-        styleHeader: { color: GREY_600 },
-        style: { fontWeight: 500 },
-        title: 'Ngày thêm mới dữ liệu',
-        dataIndex: 'bookingCode',
-        variant: 'body2',
-        render: (record: some, _index: number) => (
+        field: 'created',
+        headerName: 'Ngày thêm mới dữ liệu',
+        width: 250,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        type: 'date',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Col>
             <Typography variant="body2">
-              {moment(record?.created || record?.createAt, DATE_TIME_FORMAT).format(DATE_FORMAT)}
+              {moment(
+                params?.getValue('created') || params?.getValue('createAt'),
+                DATE_TIME_FORMAT,
+              ).format(DATE_FORMAT)}
             </Typography>
           </Col>
         ),
       },
       {
-        styleHeader: { color: GREY_600 },
-        title: 'orderCode',
-        dataIndex: 'bookingCode',
-        variant: 'body2',
-        render: (record: some, _index: number) => (
+        field: 'bookingCode',
+        headerName: intl.formatMessage({ id: 'orderCode' }),
+        width: 220,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        type: 'string',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Col>
-            <Typography variant="body2">{record?.bookingCode || record?.dataName}</Typography>
+            <Typography variant="body2">
+              {params?.getValue('bookingCode') || params?.getValue('dataName')}
+            </Typography>
           </Col>
         ),
       },
       {
-        styleHeader: { color: GREY_600 },
-        title: 'Định dạng file ảnh',
-        dataIndex: 'imageP',
-        variant: 'body2',
-        render: (_record: some, _index: number) => (
+        field: 'imageP',
+        headerName: intl.formatMessage({ id: 'Định dạng file ảnh' }),
+        width: 220,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Col>
             <Typography variant="body2">geotiff</Typography>
           </Col>
         ),
       },
       {
-        styleHeader: { color: GREY_600 },
-        title: 'Loại dữ liệu',
-        variant: 'body2',
-        render: (record: some, _index: number) => (
+        field: 'dataType',
+        headerName: intl.formatMessage({ id: 'Loại dữ liệu' }),
+        width: 220,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        type: 'string',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Col>
             <Typography variant="body2">
-              {filterList?.find((el: some) => el.id === record?.dataType)?.name}
+              {filterList?.find((el: some) => el.id === params?.getValue('dataType'))?.name}
             </Typography>
           </Col>
         ),
       },
       {
-        styleHeader: { color: GREY_600 },
-        style: { fontWeight: 500 },
-        title: 'Ngày thu nhận dữ liệu',
-        variant: 'body2',
-        render: (record: some, _index: number) => (
+        field: 'collectedDate',
+        headerName: intl.formatMessage({ id: 'Ngày thu nhận dữ liệu' }),
+        width: 250,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        type: 'date',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Col>
             <Typography variant="body2">
-              {moment(record?.collectedDate, DATE_TIME_FORMAT).format(DATE_FORMAT)}
+              {moment(params?.getValue('collectedDate'), DATE_TIME_FORMAT).format(DATE_FORMAT)}
             </Typography>
           </Col>
         ),
       },
       {
-        disableAction: true,
-        render: (record: some, _index: number) => (
+        field: 'Chỉnh Sửa / Xóa',
+        width: 220,
+        headerClassName: 'super-app-theme--header',
+        align: 'left',
+        headerAlign: 'left',
+        renderCell: (params: GridValueGetterParams | some) => (
           <Row>
             <IconButton
               style={{
@@ -115,7 +151,7 @@ const Table: React.FunctionComponent<ITableProps> = (props) => {
               }}
               onClick={() => {
                 setEditOpen(true);
-                setRowData(record);
+                setRowData(params.row);
               }}
             >
               <EditIcon />
@@ -126,7 +162,7 @@ const Table: React.FunctionComponent<ITableProps> = (props) => {
               }}
               onClick={() => {
                 setOpen(true);
-                setRowData(record);
+                setRowData(params.row);
               }}
             >
               <DeleteIcon className="svgFillAll" />
@@ -135,91 +171,62 @@ const Table: React.FunctionComponent<ITableProps> = (props) => {
         ),
       },
     ];
-    return temp as Column[];
-    // eslint-disable-next-line
   }, []);
-  console.log('dataList', dataList);
+
+  console.log('rowData', rowData);
   return (
     <React.Fragment>
-      <Formik initialValues={{}} onSubmit={() => {}}>
-        {() => (
-          <Form>
-            <div style={{ marginLeft: 12, marginTop: 10 }}>
-              <Filter
-                onUpdateFilter={(val) => {
-                  setFilter({
-                    ...filter,
-                    ...val,
-                  });
-                }}
-              ></Filter>
-            </div>
-          </Form>
-        )}
-      </Formik>
-      <TableCustom
+      <Row
         style={{
-          borderRadius: 8,
-          boxShadow: 'none',
-          padding: '16px 12px',
-          margin: '12px 12px 16px',
+          justifyContent: 'space-between',
+          padding: '4px 16px 0px 16px',
+          marginTop: 10,
         }}
-        dataSource={dataList || []}
-        columns={columns}
-        noColumnIndex
-        header={
-          <Row
-            style={{
-              justifyContent: 'space-between',
-              padding: '4px 16px 0px 16px',
-            }}
-          >
-            <Typography
-              variant="subtitle1"
+      >
+        <Typography
+          variant="subtitle1"
+          style={{
+            fontWeight: 'bold',
+          }}
+        >
+          <FormattedMessage id="listDisPlay" />
+        </Typography>
+        <LoadingButton
+          variant="contained"
+          color="secondary"
+          style={{
+            width: 220,
+            height: 36,
+            whiteSpace: 'nowrap',
+          }}
+          disableElevation
+          onClick={() => setAddOpen(true)}
+        >
+          <Row>
+            <AddIcon
               style={{
-                fontWeight: 'bold',
+                marginRight: 8,
               }}
-            >
-              <FormattedMessage id="listDisPlay" />
+            />
+            <Typography variant="body2">
+              <FormattedMessage id="addNew" />
             </Typography>
-            <LoadingButton
-              variant="contained"
-              color="secondary"
-              style={{
-                width: 180,
-                height: 36,
-                whiteSpace: 'nowrap',
-              }}
-              disableElevation
-              onClick={() => setAddOpen(true)}
-            >
-              <Row>
-                <AddIcon
-                  style={{
-                    marginRight: 8,
-                  }}
-                />
-                <Typography variant="body2">
-                  <FormattedMessage id="addNew" />
-                </Typography>
-              </Row>
-            </LoadingButton>
           </Row>
-        }
-        loading={loading}
-        paginationProps={{
-          // count: bookingDataList?.totalCount || 0,
-          count: dataList?.length || 0,
-          page: filter.pageOffset || 0,
-          rowsPerPage: filter.pageSize || 10,
-          onChangePage: (_e: unknown, newPage: number) => {
-            setFilter({ ...filter, pageOffset: newPage });
-          },
-          onChangeRowsPerPage: (e: React.ChangeEvent<HTMLInputElement>) => {
-            setFilter({ ...filter, pageSize: Number(e.target.value), pageOffset: 0 });
-          },
-        }}
-      />
+        </LoadingButton>
+      </Row>
+      <Paper style={{ margin: '16px 12px', height: 620, width: '100%' }}>
+        <DataGrid
+          rows={dataList as GridRowData[]}
+          columns={columns}
+          pageSize={10}
+          // checkboxSelection
+          disableSelectionOnClick
+          components={{
+            Toolbar: GridToolbar,
+          }}
+          loading={loading}
+        />
+      </Paper>
       <Dialog
         open={editOpen}
         onClose={() => setEditOpen(!editOpen)}
@@ -276,7 +283,7 @@ const Table: React.FunctionComponent<ITableProps> = (props) => {
                           color: 'success',
                         }),
                       );
-                    }} //''''must change edit func
+                    }}
                     disableElevation
                   >
                     <FormattedMessage id="accept" />
